f90e044c8a50084e2ca0739e7584ea53ea22849d
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2023-06-01 14:33:28 +0530

    Added solution - LeetHub

--- Check perfect square - GFG/check-perfect-square.cpp
+++ Check perfect square - GFG/check-perfect-square.cpp
@@ -1,15 +1,13 @@
 //{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h> 
+using namespace std; 
 
 // } Driver Code Ends
-// User function Template for C++
-class Solution {
-  public:
-    long long int isPerfectSquare(long long int n){
-        // code here
-        long long int tmp=sqrt(n);
+class Solution{   
+public:
+    int checkPerfectSquare(int n){
+        // code here 
+        int tmp = sqrt(n);
         if(tmp*tmp == n){
             return true;
         }
@@ -20,16 +18,17 @@ class Solution {
 };
 
 //{ Driver Code Starts.
-int main() {
+int main() 
+{ 
     int t;
-    cin >> t;
-    while (t--) {
-        long long int n;
-        cin >> n;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
         Solution ob;
-        cout << ob.isPerfectSquare(n) << endl;
+        cout << ob.checkPerfectSquare(N) << endl;
     }
-    return 0;
-}
-
+    return 0; 
+} 
 // } Driver Code Ends

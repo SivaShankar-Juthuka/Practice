c98ba0ede0a88050c1167248d2cfd31c881d4cfb
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2023-06-15 11:59:30 +0530

    Time: 223 ms (15.4%), Space: 110.3 MB (6.14%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -24,10 +24,8 @@ public:
         int pos = 0, ma = INT_MIN;
         for(int i = 0; i<v.size(); i++){
             int sum = 0;
-            for(int j = 0; j< v[i].size(); j++){
-                sum += v[i][j];
-            }
-            if(sum > ma){
+            sum = accumulate(v[i].begin(), v[i].end(), sum);
+            if(ma < sum){
                 ma = sum;
                 pos = i;
             }

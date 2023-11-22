23e8f65ace1c59af693e91f095e3ae2504ec76fa
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2023-11-22 18:36:52 +0530

    Time: 164 ms (76.07%) | Memory: 87.2 MB (68.16%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        vector<std::vector<int>> res;
+        int m = nums.size(), size = 0;
+        for(int i = 0; i < m; i++) {
+            int n = nums[i].size(), x = i;
+            for(int j = 0; j < n; j++) {
+                if(res.size() == x) {
+                    res.push_back(std::vector<int>());
+                }
+                res[x].push_back(nums[i][j]);
+                x++; size++;
+            }
+        }
+        vector<int> ans(size);
+        int idx = 0;
+        for(int i = 0; i < res.size(); i++) {
+            for(int j = res[i].size()-1; j >= 0; j--) {
+                ans[idx] = res[i][j];
+                idx++;
+            }
+        }
+        return ans;
+    }
+};

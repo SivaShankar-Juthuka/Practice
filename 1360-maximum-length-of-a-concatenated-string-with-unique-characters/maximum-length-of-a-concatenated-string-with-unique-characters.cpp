ba2981d416950bd78f6747f86943a0aa1d6dad73
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-01-23 17:55:18 +0530

    Time: 85 ms (47.36%) | Memory: 92.1 MB (24.82%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    bool verify(vector<string>res){
-        unordered_map<int, int>mp;
-        for(int i = 0; i<res.size(); i++){
-            for(int j = 0; j < res[i].size(); j ++){
-                mp[res[i][j]-'a']++;
-            }
+    int ans = 0;
+    
+    void Fun(vector<string>&arr,int ind,vector<int>v){
+        if(ind >= arr.size()){
+            int cnt = 0;
+            for(auto it:v) if(it == 1) cnt++;
+            ans = max(ans,cnt);
+            return;
         }
-        for(auto it : mp){
-            if(it.second >1) return false;
+        bool flag = true;
+        for(auto it:arr[ind]){
+            v[it-'a']++;
+            if(v[it-'a'] > 1) flag = false;
         }
-        return true;
-    }
-    int solve(int i, vector<string>&arr, vector<string>res){
-        if(i >= arr.size()) return 0;
-        res.emplace_back(arr[i]);
-        int a = 0, b = 0;
-        if(verify(res)){
-            a = arr[i].size() + solve(i + 1, arr, res);
-        }
-        res.pop_back();
-        b = solve(i+1, arr, res);
-        return max(a, b);
+        if(flag) Fun(arr,ind+1,v);
+        for(auto it:arr[ind]) v[it-'a']--;
+        Fun(arr,ind+1,v);
     }
     int maxLength(vector<string>& arr) {
-        vector<string>res;
-        return solve(0, arr, res);
+        vector<int>v(26,0);
+        Fun(arr,0,v);
+        return ans;
     }
 };

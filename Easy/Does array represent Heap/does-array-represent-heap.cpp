5ae6c1801e252ab13f90e22e645e300b4b318965
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-02-17 22:39:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Does array represent Heap/does-array-represent-heap.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+        // Your code goes here
+        int i = 0, steps = 2, j = 1, k = 2;
+        while(j != n){
+            if(arr[i] < arr[j]) return false;
+            j += 1;
+            if(j > steps){
+                steps = pow(2, k);
+                k += 1;
+                i += 1;
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       int n;
+       cin >> n;
+       int a[4*n]={0};
+       for(int i =0;i<n;i++){
+           cin >> a[i];
+       }
+       Solution ob;
+       cout<<ob.isMaxHeap(a, n)<<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends

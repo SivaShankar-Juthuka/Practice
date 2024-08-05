00a245f6b83f31789f3c59f56760313c8f199cf7
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-08-05 12:19:23 +0530

    Time: 5 ms (20.36%) | Memory: 8.3 MB (34.04%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -4,8 +4,7 @@ public:
         long long  res = 0;
         while(x) {
             if(res > INT_MAX/10 || res < INT_MIN/10)  return 0;
-            int rem = x % 10;
-            res = res * 10 + rem;
+            res = res * 10 + (x % 10);
             x /= 10;
         }
         return res;

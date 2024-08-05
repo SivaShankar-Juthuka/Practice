c813cad1ac7ee2095434a29a2e1db4a7f77d13e5
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-08-05 12:22:58 +0530

    Time: 16 ms (15.76%) | Memory: 8.4 MB (47.67%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        else{
+            long long  res = 0, n = x;
+            while(x) {
+                if(res > INT_MAX/10 || res < INT_MIN/10)  return 0;
+                int rem = x % 10;
+                res = res * 10 + rem;
+                x /= 10;
+                // cout << res << "\n";
+            }
+            return n == res;
+        }
+    }
+};

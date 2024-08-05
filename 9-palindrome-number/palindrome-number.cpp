6e57f086689ded6839cd94505a1a38cd89876c10
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-08-05 12:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (53.90%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
         else{
             long long  res = 0, n = x;
             while(x) {
-                if(res > INT_MAX/10 || res < INT_MIN/10)  return 0;
+                // if(res > INT_MAX/10 || res < INT_MIN/10)  return 0;
                 int rem = x % 10;
                 res = res * 10 + rem;
                 x /= 10;

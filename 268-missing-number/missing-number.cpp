340112fc81849da35d58686414047732455ccc72
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-08-15 16:02:11 +0530

    Time: 16 ms (38.13%) | Memory: 20.7 MB (56.54%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums)
-    {
-        if(nums.size()==1 and nums[0]==0) return 1;
-        if(nums.size()==1) return 0;
-        sort(nums.begin(),nums.end());
-        int max=nums[nums.size()-1],res,flag=0;
-        
-        for(int i=0;i<=max;i++){
-            if(i!=nums[i]){
-                res=i;
-                flag=1;
-                break;
-            }
+    int missingNumber(vector<int>& nums) {
+        if(nums.size() == 1){
+            if(nums[0] != 0) return 0;
+            else nums[0] + 1;
+        } 
+        long long mx = INT_MIN, sum = 0;
+        bool flag = false;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0) flag = true;
+            if(nums[i] > mx) mx = nums[i];
+            // mx = max(mx, nums[i]);
+            sum += nums[i];
         }
-        if(flag==1) return res;
-        else return max+1;
+        if(flag == false) return 0;
+        long long n = (mx * (mx +1)) / 2;
+        return ((n - sum) == 0)? (mx + 1): (n - sum);
     }
 };

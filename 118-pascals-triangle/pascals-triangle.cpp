a7f11c63776e68f9e35d675cc949d014f922b2bc
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-08-30 12:38:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.85%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n){
-        if(n==1) return {{1}};
-        if(n==2) return {{1},{1,1}};
-        vector<vector<int>> res;
-        res={{1},{1,1}};
-
-        for(int i=0; i<n-2; i++){
-            vector<int> temp={1};
-            for(int i=0; i<(res.back()).size()-1; i++){
-                temp.emplace_back(res.back()[i]+res.back()[i+1]);
+    vector<vector<int>> generate(int n) {
+        if(n == 1) return {{1}};
+        if(n == 2) return {{1}, {1, 1}};
+        else{
+            vector<vector<int>> res(n);
+            res[0] = {1};
+            res[1] = {1, 1};
+            for(int i = 3; i <= n; i++) {
+                res[i-1] = {};
+                int j = 0;
+                while(j < i) {
+                    if(j == 0 or j == i-1) {
+                        res[i-1].push_back(1); 
+                    }
+                    else {
+                        res[i-1].push_back(res[i-2][j-1] + res[i-2][j]);
+                    }
+                    j += 1;
+                }
             }
-            temp.emplace_back(1);
-            res.emplace_back(temp);
+            return res;
         }
-        return res;
     }
 };
